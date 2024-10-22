1da0adba86bde061265a1df9665bcb3b7bd1dcb5
Raghabendra Dash <raghabendradash779@gmail.com>
2024-10-23 02:55:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:4000', // Use a default value if VITE_API_URL is undefined
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+})
+
+
 

a61cd5919effaa269ea297aed84d9574724e3841
Raghabendra Dash <raghabendradash779@gmail.com>
2024-10-23 02:24:02 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,13 +1,12 @@
-import  { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useCallback } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
 import { toast } from "react-toastify";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const PlaceOrder = () => {
-  const navigate= useNavigate();
-
+  const navigate = useNavigate();
   const { getTotalCartAmount, token, food_list, cartItems, url } =
     useContext(StoreContext);
   const [data, setData] = useState({
@@ -28,123 +27,59 @@ const PlaceOrder = () => {
     setData((data) => ({ ...data, [name]: value }));
   };
 
-  const placeOrder = async (event) => {
+  const placeOrder = useCallback(async (event) => {
     event.preventDefault();
-    let orderItems = [];
-    food_list.map((item) => {
-      if (cartItems[item._id] > 0) {
-        let itemInfo = item;
-        itemInfo["quantity"] = cartItems[item._id];
-        orderItems.push(itemInfo);
-      }
-    });
+    let orderItems = food_list
+      .filter((item) => cartItems[item._id] > 0)
+      .map((item) => ({
+        ...item,
+        quantity: cartItems[item._id],
+      }));
+
     let orderData = {
       address: data,
       items: orderItems,
       amount: getTotalCartAmount() + 2,
     };
     
-    let response= await axios.post(url+"/api/order/place",orderData,{headers:{token}});
-    if(response.data.success){
-      const {session_url}=response.data;
+    const response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
+    if (response.data.success) {
+      const { session_url } = response.data;
       window.location.replace(session_url);
-    }else{
-      toast.error("Errors!")
+    } else {
+      toast.error("Errors!");
     }
-  };
+  }, [food_list, cartItems, data, getTotalCartAmount, token, url]);
 
-  useEffect(()=>{
-    if(!token){
-      toast.error("Please Login first")
-      navigate("/cart")
-    }
-    else if(getTotalCartAmount()===0){
+  useEffect(() => {
+    if (!token) {
+      toast.error("Please Login first");
+      navigate("/cart");
+    } else if (getTotalCartAmount() === 0) {
       toast.error("Please Add Items to Cart");
-      navigate("/cart")
+      navigate("/cart");
     }
-  },[token])
+  }, [token, getTotalCartAmount, navigate]);
+
   return (
     <form className="place-order" onSubmit={placeOrder}>
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input
-            required
-            name="firstName"
-            value={data.firstName}
-            onChange={onChangeHandler}
-            type="text"
-            placeholder="First name"
-          />
-          <input
-            required
-            name="lastName"
-            value={data.lastName}
-            onChange={onChangeHandler}
-            type="text"
-            placeholder="Last name"
-          />
+          <input required name="firstName" value={data.firstName} onChange={onChangeHandler} type="text" placeholder="First name" />
+          <input required name="lastName" value={data.lastName} onChange={onChangeHandler} type="text" placeholder="Last name" />
         </div>
-        <input
-          required
-          name="email"
-          value={data.email}
-          onChange={onChangeHandler}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          required
-          name="street"
-          value={data.street}
-          onChange={onChangeHandler}
-          type="text"
-          placeholder="Street"
-        />
+        <input required name="email" value={data.email} onChange={onChangeHandler} type="text" placeholder="Email Address" />
+        <input required name="street" value={data.street} onChange={onChangeHandler} type="text" placeholder="Street" />
         <div className="multi-fields">
-          <input
-            required
-            name="city"
-            value={data.city}
-            onChange={onChangeHandler}
-            type="text"
-            placeholder="City"
-          />
-          <input
-            required
-            name="state"
-            value={data.state}
-            onChange={onChangeHandler}
-            type="text"
-            placeholder="State"
-          />
+          <input required name="city" value={data.city} onChange={onChangeHandler} type="text" placeholder="City" />
+          <input required name="state" value={data.state} onChange={onChangeHandler} type="text" placeholder="State" />
         </div>
         <div className="multi-fields">
-          <input
-            required
-            name="zipcode"
-            value={data.zipcode}
-            onChange={onChangeHandler}
-            type="text"
-            placeholder="Zip Code"
-          />
-          <input
-            required
-            name="country"
-            value={data.country}
-            onChange={onChangeHandler}
-            type="text"
-            placeholder="Country"
-          />
+          <input required name="zipcode" value={data.zipcode} onChange={onChangeHandler} type="text" placeholder="Zip Code" />
+          <input required name="country" value={data.country} onChange={onChangeHandler} type="text" placeholder="Country" />
         </div>
-        <input
-          required
-          name="phone"
-          value={data.phone}
-          onChange={onChangeHandler}
-          type="text"
-          placeholder="Phone"
-        />
+        <input required name="phone" value={data.phone} onChange={onChangeHandler} type="text" placeholder="Phone" />
       </div>
       <div className="place-order-right">
         <div className="cart-total">
@@ -162,9 +97,7 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>
-                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
-              </b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
           <button type="submit">PROCEED TO PAYMENT</button>

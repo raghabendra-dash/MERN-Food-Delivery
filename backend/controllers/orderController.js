368aeaee5a6d2b1ee34bc61999182d505c6a5f31
Raghabendra Dash <raghabendradash779@gmail.com>
2025-04-11 14:55:43 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://food-delivery-website-hazel.vercel.app/";
+  const frontend_url = "https://tomato-food-delivery-tanmay-312s-projects.vercel.app";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,

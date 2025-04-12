390386aa6a58a0a7d6664109686e978e7121e6c1
Raghabendra Dash <raghabendradash779@gmail.com>
2025-04-12 21:45:36 +0530

    Update cartController.js

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,6 +1,5 @@
 import userModel from "../models/userModel.js";
 
-// add items to user cart
 const addToCart = async (req, res) => {
   try {
     let userData = await userModel.findById(req.body.userId);
@@ -18,7 +17,6 @@ const addToCart = async (req, res) => {
   }
 };
 
-// remove from cart
 const removeFromCart = async (req, res) => {
   try {
     let userData = await userModel.findById(req.body.userId);
@@ -36,7 +34,6 @@ const removeFromCart = async (req, res) => {
   }
 };
 
-// fetch user cart data
 const getCart = async (req, res) => {
   try {
     let userData = await userModel.findById(req.body.userId);
